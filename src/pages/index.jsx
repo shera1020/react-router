283f16e073d5ae1a443f6057e60faeedd98c3947
shera1020 <52871541+shera1020@users.noreply.github.com>
2020-10-01 14:41:48 -0400

    Link change in index file

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,9 +4,10 @@ import { Link } from "react-router-dom";
 const MainPage = () => {
   return (
     <div>
+      <Link to="/users">Show a list of name</Link>
       <h3>Welcome to React Router Tutorial.</h3>
       <small> Main Page</small>
-      <Link to="/users">Show a list of name</Link>
+      
     </div>
   );
 };
